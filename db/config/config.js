dfa856b410c5c14dcd2429865f644895cf34ebdb
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-03 00:28:45 +0530

    chore: initialize repo & create directory structure

---
+++ db/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": "database_dev",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+  },
+  "test": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+  },
+  "production": {
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": "database_prod",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+  }
+}
