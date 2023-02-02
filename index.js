8b5be3f252618eb53f65ed27ee2e4b1909fa20f5
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-03 01:22:09 +0530

    feat: create basic server without any endpoint

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const {PORT} = require('./config');
+const routes = require('./src/routes');
+
+const app = express();
+
+app.use(express.json());
+app.use('', routes);
+
+
+app.listen(PORT, () => {
+	console.log(`open server on http://localhost:${PORT}`);
+});
