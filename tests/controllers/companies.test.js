855d40f4c482653f6ada31e3caa3aa58e1921b7e
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-02-05 19:12:57 +0530

    chore: add test for controllers

--- tests/controllers/companies.test.js
+++ tests/controllers/companies.test.js
@@ -1,4 +1,4 @@
-const { getCompanyList } = require('../../src/controllers/companies');
+const { getCompanyList, patchCompanyById} = require('../../src/controllers/companies');
 const companyServices = require('../../src/services/companies');
 
 describe('getCompanyList', () => {
@@ -35,5 +35,61 @@ describe('getCompanyList', () => {
 
   it ('should call getCompanyListRankedByScore with sector', async () => {
     expect(mockGetCompanyListRankedByScore).toHaveBeenCalledWith(req.query.sector);
+  });
+
+  it('should call next with error', async () => {
+    const error = new Error('error');
+    mockGetCompanyListRankedByScore.mockRejectedValue(error);
+    await getCompanyList(req, res, next);
+    expect(next).toHaveBeenCalledWith(error);
+  });
+});
+
+// create test  for patchCompanyById controller with mocking
+describe('patchCompanyById', () => {
+  const req = {
+    params: {
+      id: '1'
+    },
+    body: {
+      'ceo': 'CEO A',
+      'name': 'Company A'
+    }
+  };
+
+  const res = {
+    json: jest.fn()
+  };
+
+  const next = jest.fn();
+
+  const mockedPatchCompanyIdService = jest.spyOn(companyServices, 'patchCompayById').mockResolvedValue({
+    'id': '1',
+    'name': 'Company A',
+    'ceo': 'CEO A',
+    'score': 11
+  });
+
+  patchCompanyById(req, res, next);
+
+  it('should execute res.json', async () => {
+    expect(res.json).toHaveBeenCalledWith({
+      'id': '1',
+      'name': 'Company A',
+      'ceo': 'CEO A',
+      'score': 11
+    });
+  });
+
+  it('should call patchCompanyIdService with id, ceo, name', async () => {
+    expect(mockedPatchCompanyIdService)
+      .toHaveBeenCalledWith(req.params.id, req.body.ceo, req.body.name);
+  });
+
+  it('should call next with error', async () => {
+    const error = new Error('error');
+    mockedPatchCompanyIdService.mockRejectedValue(error);
+    await patchCompanyById(req, res, next);
+    expect(next).toHaveBeenCalledWith(error);
   });
 });
